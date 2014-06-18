d6fdf24714eb2519bed021623bd1e75d1ec316b7
sla89 <skullsplitter@die-optimisten.net>
2014-06-18 09:21:57 +0200

    init csharp projects and added udp/tcp echo test clients

---
+++ TCPEchoClient/TCPEchoClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPEchoClient
+{
+    class TCPEchoClient
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+                IPAddress ip = new IPAddress(new byte[] { 192, 168, 0, 7 });
+
+                tcpclnt.Connect(ip, 2000);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                Console.WriteLine("Received: ");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UDPEchoClient/UDPEchoClient.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPEchoClient
+{
+    class UDPEchoClient
+    {
+        private static int PORT = 2000;
+
+        static void Main(string[] args)
+        {
+            //create a new server
+            var server = new UdpListener();
+
+            //start listening for messages and copy the messages back to the client
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    var received = await server.Receive();
+                    Console.WriteLine("received: " + received.Message);
+                }
+            });
+
+            //create a new client
+            var client = UdpUser.ConnectTo("192.168.0.7", PORT);
+
+            //wait for reply messages from server and send them to console 
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var received = await client.Receive();
+                        Console.WriteLine(received.Message);
+                        if (received.Message.Contains("quit"))
+                            break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write(ex);
+                    }
+                }
+            });
+
+            //type ahead :-)
+            string read;
+            do
+            {
+                read = Console.ReadLine();
+                client.Send(read);
+            } while (read != "quit");
+        }
+
+        public struct Received
+        {
+            public IPEndPoint Sender;
+            public string Message;
+        }
+
+        abstract class UdpBase
+        {
+            protected UdpClient Client;
+
+            protected UdpBase()
+            {
+                Client = new UdpClient();
+            }
+
+            public async Task<Received> Receive()
+            {
+                var result = await Client.ReceiveAsync();
+                return new Received()
+                {
+                    Message = Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length),
+                    Sender = result.RemoteEndPoint
+                };
+            }
+        }
+
+        //Server
+        class UdpListener : UdpBase
+        {
+            private IPEndPoint _listenOn;
+
+            public UdpListener()
+                : this(new IPEndPoint(IPAddress.Any, PORT))
+            {
+            }
+
+            public UdpListener(IPEndPoint endpoint)
+            {
+                _listenOn = endpoint;
+                Client = new UdpClient(_listenOn);
+            }
+
+            public void Reply(string message, IPEndPoint endpoint)
+            {
+                var datagram = Encoding.ASCII.GetBytes(message);
+                Client.Send(datagram, datagram.Length, endpoint);
+            }
+
+        }
+
+        //Client
+        class UdpUser : UdpBase
+        {
+            private UdpUser() { }
+
+            public static UdpUser ConnectTo(string hostname, int port)
+            {
+                var connection = new UdpUser();
+                connection.Client.Connect(hostname, port);
+                return connection;
+            }
+
+            public void Send(string message)
+            {
+                var datagram = Encoding.ASCII.GetBytes(message);
+                Client.Send(datagram, datagram.Length);
+            }
+
+        }
+    }
+}
